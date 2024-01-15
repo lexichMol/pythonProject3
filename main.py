3cffc53a9799592cbaff1d0a4fc75dc9b783b5b8
lexichMol <123189889+lexichMol@users.noreply.github.com>
2024-01-15 21:31:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,94 +1,36 @@
-from kivymd.app import MDApp
-from kivy.app import App
-from kivy.uix.camera import Camera
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', "POST"])
+def registration():
+    if (request.form.get("I") != None or request.form.get("T") != None) and request.form.get("name") != None:
+
+        return redirect(url_for("task"))
+    print(request.form.get("name"))
+    print(request.form.get("I"))
+    print(request.form.get("T"))
+    return render_template("index.html")
+
+@app.route("/task", methods=['GET', "POST"])
+def task():
+
+    if request.form.get("intest1") == "10" and request.form.get("intest2") == "5":
+        return redirect(url_for("ok"))
+    elif request.form.get("intest1") != None and request.form.get("intest2") != None:
+        return redirect(url_for("no"))
+
+    return render_template("task.html")
+
+@app.route("/ok")
+def ok():
+    return render_template("ok.html")
+
+@app.route("/no")
+def no():
+    return render_template("no.html")
 
 
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.uix.image import Image, AsyncImage
 
-from deepface import DeepFace
-from kivymd.uix.label import MDLabel
-import os
-import glob
-from kivy.config import Config
-Config.set('graphics', 'width', '500')
-Config.set('graphics', 'height', '800')
-
-
-
-class CameraExample(MDApp, App):
-    def build(self):
-        self.count_img = 0
-        self.layout = FloatLayout()
-
-        self.text = MDLabel(text="подобрать фильм по настроению", size_hint=(.6, .1), pos_hint={'x': .3, 'y': .85})
-
-        self.cameraObject = Camera(play=False)
-        self.cameraObject.play = True
-        self.cameraObject.resolution = (400, 300)  # Specify the resolution
-
-
-        self.take_photo = Button(text='Сделать\n  Фото', size_hint=(.3, .07), pos_hint={'x': .35, 'y': .1})
-        self.take_photo.bind(on_press=self.onCameraClick)
-
-        self.layout.add_widget(self.text)
-        self.layout.add_widget(self.cameraObject)
-        self.layout.add_widget(self.take_photo)
-
-        return self.layout
-
-    def onCameraClick(self, *args):
-        self.cameraObject.export_to_png('img/selfie'+str(self.count_img)+'.png')
-        self.emotion()
-        setattr(self.cameraObject, 'opacity', 0)
-
-
-        self.done = Button(text='Готово', size_hint=(.2, .07), pos_hint={'x': .1, 'y': .1})
-        self.done.bind(on_press=self.okeyy)
-        self.layout.add_widget(self.done)
-
-
-        self.take_photo_omt = Button(text='Сделать фото\n   ещё раз', size_hint=(.3, .07), pos_hint={'x': .65, 'y': .1})
-        self.take_photo_omt.bind(on_press=self.change)
-
-        self.layout.add_widget(self.take_photo_omt)
-        self.img = Image(source='img/selfie'+str(self.count_img)+'.png')
-        self.layout.add_widget(self.img)
-        self.layout.remove_widget(self.take_photo)
-
-        return self.layout
-
-    def emotion(self):
-        try:
-            result_dict = DeepFace.analyze(img_path='img/selfie'+str(self.count_img)+'.png', actions=['emotion'])
-            self.layout.remove_widget(self.text)
-            self.text = MDLabel(text=result_dict[0].get('dominant_emotion'), size_hint=(.6, .1), pos_hint={'x': .3, 'y': .85})
-            print(result_dict[0].get('dominant_emotion'))
-            self.layout.add_widget(self.text)
-            return self.layout
-
-        except Exception as _ex:
-            return _ex
-    def change(self, *args):
-        self.count_img += 1
-        setattr(self.cameraObject, 'opacity', 1)
-        self.layout.remove_widget(self.img)
-        self.layout.remove_widget(self.done)
-        self.layout.remove_widget(self.take_photo_omt)
-        self.take_photo = Button(text='Сделать\n  Фото', size_hint=(.3, .07), pos_hint={'x': .35, 'y': .1})
-        self.take_photo.bind(on_press=self.onCameraClick)
-
-        self.layout.add_widget(self.take_photo)
-
-    def okeyy(self, *args):
-        files = glob.glob('img/*')
-        for f in files:
-            os.remove(f)
-
-
-# Start the Camera App
-
-if __name__ == '__main__':
-    CameraExample().run()
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
